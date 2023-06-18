53d35ffef879bd206a1cdde21cc0725f9fd8e457
ivyy-poison <tan.yuhao.ivan@gmail.com>
2023-06-18 16:27:15 +0800

    implement live update of user profile after changes

--- frontend/components/base/EditProfilePicButton/CropperComponent.tsx
+++ frontend/components/base/EditProfilePicButton/CropperComponent.tsx
@@ -4,6 +4,8 @@ import EditPicButton from './EditPicButton';
 import ImageCropper from './ImageCropper';
 import axios from 'axios';
 import { User } from '../../../types';
+import { useUser } from '../../../userContext';
+
 
 interface CropperComponentProps {
     profilePic?: string;
@@ -14,6 +16,7 @@ const CropperComponent: React.FC<CropperComponentProps> = ({profilePic, setUser}
     const { isOpen, onOpen, onClose } = useDisclosure();
     const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
     const toast = useToast();
+    const { setNeedUpdate } = useUser();
   
     const handleValidFile = (file: File) => {
         setSelectedImage(URL.createObjectURL(file));
@@ -21,7 +24,6 @@ const CropperComponent: React.FC<CropperComponentProps> = ({profilePic, setUser}
     };
   
     const handleCroppedImage = (dataUrl: string) => {
-        console.log(dataUrl);
         const url = "http://localhost:8080/auth/user"
         axios.patch(url, {
                 profilepic: dataUrl,
@@ -29,15 +31,15 @@ const CropperComponent: React.FC<CropperComponentProps> = ({profilePic, setUser}
                 withCredentials: true,
             })
             .then(res => {
-                console.log(res.data); // Log the response data
                 const { ProfilePic } = res.data.data;
                 setUser((prevUser: User) => ({
                     ...prevUser,
                     ProfilePic: ProfilePic
                 }));
+                setNeedUpdate(true);
                 toast({
                     title: "Profile updated.",
-                    description: "We've updated your profile for you.",
+                    description: "We've updated your profile picture for you.",
                     status: "success",
                     duration: 9000,
                     isClosable: true,
